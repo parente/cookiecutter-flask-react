35bd6fea1c7c377bd3055ac2464876ab8578aa3c
Justin Smith <smithjustinj1@gmail.com>
2018-12-21 18:34:38 +0000

    Minor edits from read through

--- {{cookiecutter.app_slug}}/{{cookiecutter.app_module}}/__init__.py
+++ {{cookiecutter.app_slug}}/{{cookiecutter.app_module}}/__init__.py
@@ -1,5 +1,5 @@
 # These three lines comes from versioneer.
-# Add to this file if you want, but do remove these.
+# Add to this file if you want, but do not remove these.
 from ._version import get_versions
 __version__ = get_versions()['version']
 del get_versions
--- {{cookiecutter.app_slug}}/{{cookiecutter.app_module}}/api.py
+++ {{cookiecutter.app_slug}}/{{cookiecutter.app_module}}/api.py
@@ -1,5 +1,4 @@
 import datetime
-import glob
 import json
 import os
 
